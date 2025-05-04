36a57a2b59d23cb0609c273d1ae170ee385c1b3a
Satzhan7 <sissenbaiuly@gmail.com>
2025-05-04 05:26:08 +0500

    chat api

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
-// import 'vuetify/styles'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
